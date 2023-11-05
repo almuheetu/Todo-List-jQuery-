701eb41c6e9d3442ab130ece598b461be0cd4c3d
Almuheetu Shihab <almuheetu@gmail.com>
2023-11-05 14:40:24 +0600

    jquery fixed

--- main.js
+++ main.js
@@ -5,3 +5,9 @@ $('input').keypress(function(event) {
         $('ul').append('<li>' + todoText + '<span><i class="fa-solid fa-trash"></i></span>')
     }
 });
+$('ul').on('click', "span" , function(event) {
+    $(this).parent().fadeOut(500,function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
